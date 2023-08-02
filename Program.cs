54f1fa69e7b2c7ed864d84e711d593060818f2f1
Nikita Sivakumar <nikita.siva@gmail.com>
2023-08-01 22:48:17 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ObserverPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WeatherProvider wp = new WeatherProvider();
+            WeatherStation ob = new WeatherStation(wp);
+            wp.SetMeasurements();
+            ob.Display();
+
+            wp.SetMeasurements();
+            ob.Display();
+        }
+    }
+}
---
+++ WeatherStation.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace ObserverPattern
+{
+    public class WeatherStation : IObserver, IDisplay
+    {
+        private ISubject wp;
+        private int temp;
+        private int pressure;
+        private int humidity;
+
+
+        public WeatherStation(ISubject weatherProvider)
+        {
+            this.wp = weatherProvider;
+            this.wp.RegisterObserver(this);
+        }
+
+        public void Update(int temp, int pressure, int humidity)
+        {
+            this.temp = temp;
+            this.pressure = pressure;
+            this.humidity = humidity;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine(String.Format("T:{0}, P:{1}, H:{2}", this.temp.ToString(), this.pressure.ToString(), this.humidity.ToString()));
+        }
+    }
+}
