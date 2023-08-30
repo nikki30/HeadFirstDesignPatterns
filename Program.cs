f5e2835607bd7f24baf7ede4ae50af89494954a1
Nikita Sivakumar <nikita.siva@gmail.com>
2023-08-29 23:36:56 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var obj = SingletonClass.GetSingletonObject();
+            Console.WriteLine(SingletonClass.GetProperties(obj));
+        }
+    }
+}
---
+++ SingletonClass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace Singleton
+{
+    public class SingletonClass
+    {
+        // Instantiating it statically eagerly (instead of the commented code)
+        // ensures no multi-threading cases where two objects are created during parallel null checks being passed (only occurs during first time obj is created).
+        private static SingletonClass singletonObj = new SingletonClass();
+        private SingletonClass()
+        {
+        }
+
+        public static SingletonClass GetSingletonObject()
+        {
+            // singletonObj ??= new SingletonClass();
+            return singletonObj;
+        }
+
+        public static string GetProperties(SingletonClass obj)
+        {
+            return "so many properties";
+        }
+    }
+}
